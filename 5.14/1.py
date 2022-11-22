74d51c3615a80412d83dcef426bf9b4624953852
evelinhollik <hollik.evelin@gmail.com>
2022-11-22 14:58:11 +0100

    f

--- 5.14/1.py
+++ 5.14/1.py
@@ -1,5 +1,7 @@
-napok = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"]
-nap = int(input("Hány napra mész el? "))
-szam=0
-szam= nap%7
-print(nap[szam-1])
+napok = [ "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap" ]
+
+try:
+    napIndex = int(input("Hány napra mész el? "))
+    print(napok[napIndex - 1])
+except:
+    print("Nem helyes formátum!")
