6a3b4856536eaa4868fcf3e5bfd0294720848584
EvelinHollik <hollik.evelin@gmail.com>
2022-11-29 09:33:49 +0100

    vgh

---
+++ 6.9/1.py
@@ -0,0 +1,16 @@
+egtaj=("É", "K", "D", "NY")
+kovetkezo=""
+
+def fordulj_orajarasi_iranyba(egtaj):
+    if egtaj =="É":
+        kovetekezo="Kelet"
+    elif egtaj == "K":
+        kovetkezo ="Dél"
+    elif egtaj == "D":
+        kovetkezo ="Nyugat"
+    elif egtaj == "NY":
+        kovetkezo ="Észak"
+    print("Add meg az égtájat:")
+
+fordulj_orajarasi_iranyba()
+    
