13b0a24f945d90c625b572d2b439b0b66561d2ef
EvelinHollik <hollik.evelin@gmail.com>
2022-11-29 11:19:07 +0100

    gr

--- 6.9/1.py
+++ 6.9/1.py
@@ -1,3 +1,4 @@
+#1
 class teszt:
     def irany(irany):
         if irany == "é" and "É":
@@ -13,3 +14,47 @@ class teszt:
         return irany
 
 teszt.irany("é")
+
+#2
+class teszt:
+    def nap_nev(nap_nev):
+        int(input("Adj meg egy számot: "))
+        if nap_nev == "0":
+            print("Hétfő")
+        if nap_nev == "1":
+            print("Kedd")
+        if nap_nev == "2":
+            print("Szerda")
+        if nap_nev == "3":
+            print("Csütörtök")
+        if nap_nev == "4":
+            print("Péntek")
+        if nap_nev == "5":
+            print("Szombat")
+        if nap_nev == "6":
+            print("Vasárnap")
+        if nap_nev >= "7":
+            print("None")
+ 
+teszt.nap_nev()
+
+#3
+class teszt:
+    def nap_nev(nap_nev):
+        int(input("Adj meg egy napot: "))
+        if nap_nev == "Hétfő":
+            print("0")
+        if nap_nev == "Kedd":
+            print("1")
+        if nap_nev == "Szerda":
+            print("2")
+        if nap_nev == "Csütörtök":
+            print("3")
+        if nap_nev == "Péntek":
+            print("4")
+        if nap_nev == "Szombat":
+            print("5")
+        if nap_nev == "Vasárnap":
+            print("6")
+
+teszt.nap_nev()
