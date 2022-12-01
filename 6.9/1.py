c8d1a36d4bea3ff67988ffe6aa323ff65d7c1696
EvelinHollik <hollik.evelin@gmail.com>
2022-12-01 11:50:30 +0100

    dfs

--- 6.9/1.py
+++ 6.9/1.py
@@ -17,41 +17,43 @@ teszt.irany("é")
 #2
 class teszt:
     def nap_nev(nap_nev):
-        int(input("Adj meg egy számot: "))
         if nap_nev == "0":
             print("Hétfő")
-        if nap_nev == "1":
+        elif nap_nev == "1":
             print("Kedd")
-        if nap_nev == "2":
+        elif nap_nev == "2":
             print("Szerda")
-        if nap_nev == "3":
+        elif nap_nev == "3":
             print("Csütörtök")
-        if nap_nev == "4":
+        elif nap_nev == "4":
             print("Péntek")
-        if nap_nev == "5":
+        elif nap_nev == "5":
             print("Szombat")
-        if nap_nev == "6":
+        elif nap_nev == "6":
             print("Vasárnap")
-        if nap_nev >= "7":
+        elif nap_nev >= "7":
             print("None")
-teszt.nap_nev(7)
+teszt.nap_nev(input("Adj meg egy számot: "))
+
 
 #3
-class teszt:
+
+class teszt1:
     def nap_nev(nap_nev):
-        int(input("Adj meg egy napot: "))
         if nap_nev == "Hétfő":
             print("0")
-        if nap_nev == "Kedd":
+        elif nap_nev == "Kedd":
             print("1")
-        if nap_nev == "Szerda":
+        elif nap_nev == "Szerda":
             print("2")
-        if nap_nev == "Csütörtök":
+        elif nap_nev == "Csütörtök":
             print("3")
-        if nap_nev == "Péntek":
+        elif nap_nev == "Péntek":
             print("4")
-        if nap_nev == "Szombat":
+        elif nap_nev == "Szombat":
             print("5")
-        if nap_nev == "Vasárnap":
+        elif nap_nev == "Vasárnap":
             print("6")
-teszt.nap_nev(7)
+        else:
+            print("None")
+teszt1.nap_nev(input("Adj meg egy napot: "))
